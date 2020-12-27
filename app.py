b6a16eab1c1040e482b3fd537fb7f0bed8875123
Luis Larota <lflarota@uc.cl>
2020-12-27 12:59:34 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,233 @@
+import streamlit as st
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go 
+from scipy import spatial
+from statistics import mode 
+import pandas as pd 
+import back
+import re
+from plotly.subplots import make_subplots
+global colors
+colors = ["#F9E79F", '#82E0AA', '#922B21', '#08056B', '33FFF4', 'FC33FF', 'FF8333', '070300']
+#ranges = [[-1000000,0], [0,30000], [30000,1000000]]
+rang_def = [[0,0.3], [0.3,0.6], [0.6,1], [1,10]]
+def main():
+    st.set_option('deprecation.showfileUploaderEncoding', False)
+    st.image('logo.jpg')
+
+    st.sidebar.write('Do you have a block model file?')
+    say_yes = st.sidebar.checkbox('Yes')
+    say_no = st.sidebar.checkbox('No')
+    if say_yes and say_no:
+        st.sidebar.success('Please choose only one option')
+    elif say_no:
+        #if st.sidebar.radio('',['Choose default file']):
+        st.sidebar.success('Loading default file')
+        load_file('nuevog.csv')
+    elif say_yes:
+        file_= st.sidebar.file_uploader('*Upload or drop the file:')
+        if file_:
+            load_file(file_)
+
+
+def load_file(file_model):
+    if file_model:
+        
+        separator = st.sidebar.radio('*csv file delimiter:', (',', ';'))
+        if separator is not None:
+            #We are calling the block model here:
+            model = back.blockmodel(pd.read_csv(file_model, skipinitialspace= True, sep = separator))    
+            x = st.sidebar.selectbox('X coordinate:', model.columns())
+            y = st.sidebar.selectbox('Y coordinate:', model.columns())
+            z = st.sidebar.selectbox('Z coordinate:', model.columns())
+            grade = st.sidebar.selectbox('Main Grade:', model.columns())
+            density = st.sidebar.selectbox('Density:', model.columns())
+            
+            #Load archive:
+            if st.sidebar.checkbox('Load'):
+                analize(model, x,y,z, grade, density)
+
+        
+
+def analize(model,x,y,z, grade,density):
+    st.success('Based on your file:')
+    model.summary(x,y,z, grade, density)()
+    st.markdown('I got:')
+    st.write('{} blocks in X'.format(model.xlong))
+    st.write('{} blocks in Y'.format(model.ylong))
+    st.write('{} blocks in Z'.format(model.zlong))
+    total_block_times = model.long
+    if total_block_times != model.summary_2():
+        st.success('Message: You need to have {} blocks. However, you got {} blocks'.format(total_block_times, model.bmodel.shape[0]))
+        if st.checkbox('Check outliers and coordinates that are not in the block center:'):
+            #Cleanning gets [outliers]
+            outliers = model.cleanning()
+            st.write('I am taking out the following rows:')
+            st.dataframe(outliers)
+            
+            #Asking one more time, if it wants to re-analyze de file
+            analyze_again = st.radio('Analyze again?:', ['No', 'Yes'])
+            if analyze_again == 'Yes':
+                st.markdown('<i class="material-icons">If you can not move forward, pls check your csv file by your own!</i>', unsafe_allow_html=True)
+                model = back.blockmodel(model.bmodel)
+                analize(model, x,y,z, grade, density)
+            
+    #If number of blocks = to unique values on x,y,z then:
+    elif total_block_times == model.summary_2():
+        st.success('Based on the file, # blocks = # rows')
+        #Calling visualizer that needs more development (blocks sizing)
+        if st.checkbox('Visualize block model'):
+            visualize_model(model, x,y,z, grade, density)
+        if st.checkbox('Get Grade-Tonnage Distribution'):
+            if type(grade) == str:
+                grade = [grade]
+            #Choosing main grade for the exercise
+            select_mgrade = st.selectbox('Select your main grade:', grade)
+            st.plotly_chart(call_grade_tonnage(model, select_mgrade, density))   
+            if st.checkbox('Get the Ultimate Pit Limit'):
+                m_c = st.number_input('Mining Cost (USD/Ton.)')  #(USD/ Ton.)
+                opt_increm = st.selectbox('Incremental Cost?', ['Yes', 'No'])
+                if opt_increm == 'Yes':    
+                    i_c  = st.text_input('Incremental Cost (USD/ton.)') #(USD/ Ton./ meter deep)
+                else:
+                    i_c = 0
+        
+                p_c  = st.number_input('Processing Cost (USD/ton.)')  #(USD/ Ton.)
+                t_c = st.number_input('Treatment Cost (USD/lb.)')  #(USD/ lb.)
+                m_p = st.number_input('Metal Price (USD/lb.)')  #(USD/ lb.)
+                m_r = st.number_input('Metal Recovery (<1)') #
+                prec = st.selectbox('Select block precedences:', ['1-5 pattern','1-9 pattern'], key= 'the_prec')
+
+                if st.button('Solve the Ultimate Pit Limit problem'):
+                    model_solve = model.upl(x,y,z, select_mgrade, density, m_c, float(i_c), p_c, t_c, m_p, m_r, prec)
+                    return st.plotly_chart(visualize_upl(model_solve, x,y,z, select_mgrade, rang_def, colors))
+
+def visualize_model(model, x,y,z, grade, density):
+    x_slider =  st.slider('X Range:', model.minx, model.maxx, (model.minx, model.minx + 5*model.modex), model.modex)
+    y_slider =  st.slider('Y Range:', model.miny, model.maxy, (model.miny, model.miny + 5*model.modey), model.modey)
+    z_slider =  st.slider('Z Range:', model.minz, model.maxz, (model.minz, model.minz + 5*model.modez), model.modez)
+
+    text_1 = 'Select ranges with 1 decimal  -   i.e., 0.1, 0.2, 0.3, 2.0 (max. 8 numbers)'
+    p_compile = re.compile(r'\d+\.\d+')
+
+    #st.success(text_1)
+    st.success(text_1)
+    min_grade = round(min(model.bmodel.loc[:, grade]),3)
+    max_grade = round(max(model.bmodel.loc[:, grade]),3)
+    ranges = st.text_input('Minimun {} grade is: {}, and maximum one is: {}'.format(grade, min_grade, max_grade))
+    # Compile a pattern to capture float values
+    # Convert strings to float
+    # Asking to visualize
+    ask_visualize = st.radio('Visualize:', ['No', 'Yes'])
+    if ask_visualize == 'Yes':
+        floats = [float(i) for i in p_compile.findall(ranges)]
+        floats = list_maker(floats)
+        st.plotly_chart(visualize(model.bmodel, x,y,z, x_slider, y_slider, z_slider, grade, floats, density, colors))
+
+#Visualizing the whole, remember to use . as decimal separator
+def visualize(model, x,y,z, x_slider, y_slider, z_slider, grade, floats, density, colors):
+    i = 0
+    for_plot = model.loc[((model.loc[:, x]>= x_slider[0]) & (model.loc[:,x] <= x_slider[1])) & \
+        ((model.loc[:,y]>= y_slider[0]) & (model.loc[:,y] <= y_slider[1]))&\
+            ((model.loc[:,z]>= z_slider[0]) & (model.loc[:,z] <= z_slider[1]))]
+    fig = go.Figure()
+    #set ranges with colors:
+    for value in floats:
+        before = value[0]
+        after = value[1]
+        data_plot = for_plot.loc[(for_plot.loc[:,grade]>= before) & (for_plot.loc[:,grade]< after)]
+        data_plot.loc[:, grade] =data_plot.loc[:, grade].round(3)
+        data_plot.loc[:, 'g'] = '{}: '.format(grade) + data_plot.loc[:, grade].astype(str)
+        #naming the plot on the legend
+        name_legend = str(before) + " <=" + grade +"< " +str(after)
+        fig.add_trace(go.Scatter3d(x = data_plot.loc[:, x], y = data_plot.loc[:,y], z = data_plot.loc[:,z], text= data_plot.loc[:,'g'],mode = 'markers'\
+                                , name = name_legend, marker = dict(color = colors[i], symbol = 'square', size = 4), showlegend = True))
+        i+=1
+        title = '<b>Block model for {} grade</b>'.format(grade)
+
+    fig.update_layout(margin = dict(r=100, t=25, b=40, l=100), title = title)
+    
+    return fig
+
+#Grade-tonnage curve
+def call_grade_tonnage(model, grade, density):
+    
+    volume = model.modex * model.modey * model.modez
+    modelo = model.bmodel
+    modelo.loc[:,'ton'] = volume * modelo.loc[:,density]
+    modelo.loc[:,'ton_g'] = modelo.loc[:,'ton']  * modelo.loc[:,grade]/100
+
+    cutoff_grade = list(np.arange(0,2, 0.05))
+    ton_a_cutoff = []
+    ton_cutfine = []
+    for i in cutoff_grade:
+        x = modelo[modelo.loc[:, grade]>=i]['ton'].sum()/1000000000
+        y = modelo[modelo.loc[:, grade]>=i]['ton_g'].sum()/1000000000
+        ton_a_cutoff.append(x)
+        ton_cutfine.append(y)
+    grade_ton_dist = pd.DataFrame({'Cutoff_grade': cutoff_grade, 'GTon_a_cutoff': ton_a_cutoff, 'GTon_cutfine' : ton_cutfine})
+    grade_ton_dist['Cut_Grade'] = grade_ton_dist['GTon_cutfine']*100/grade_ton_dist['GTon_a_cutoff']
+    title = '<b>Grade - Tonnage Distribution for {} grade</b>'.format(grade)
+    #8 We can plot it .. so now it may be familiar to you (PD: Forgot to grab some packages)
+    fig1 = make_subplots(specs=[[{"secondary_y": True}]])
+    fig1.add_trace(go.Scatter(x = grade_ton_dist['Cutoff_grade'], y = grade_ton_dist['GTon_a_cutoff'], name = 'Tonnage (Gtons.)  vs. Cut-off grade (%)'), secondary_y = False) 
+    fig1.add_trace(go.Scatter(x = grade_ton_dist['Cutoff_grade'], y = grade_ton_dist['Cut_Grade'], name = 'Avg. grade (%) vs Cut-off grade (%)'), secondary_y = True)
+    fig1.update_xaxes(title_text = "<b>Cut-off grade (%)<b>")
+    fig1.update_yaxes(title_text="<b>Tonnage (Gtons.)</b>", secondary_y =False)
+    fig1.update_yaxes(title_text="<b>Avg. grade (%)</b>", secondary_y =True)
+    fig1.update_layout(title = title)
+    model.bmodel = modelo
+    return fig1
+
+def list_maker(lista):
+    list_ranges = []
+    for i in range(len(lista)):
+        if i == len(lista)-1:
+            break
+        else:
+            bef = lista[i]
+            after = lista[i+1]
+            list_ranges.append([bef, after])
+    return list_ranges
+
+
+
+
+def visualize_upl(model, xcol, ycol, zcol, grade, ranges, colors):
+    i = 0
+    fig = make_subplots(rows = 6, cols = 2, specs = [[{'type': 'scatter3d', 'rowspan':4, 'colspan':2}, None],
+                                                [None, None],
+                                                [None, None],
+                                                [None, None],
+                                                [{'type': 'bar', 'rowspan':2,'colspan':2}, None],
+                                                [None, None]])
+    #set ranges with colors:
+    for value in ranges:
+        before = value[0]
+        after = value[1]
+        data_plot = model[(model.loc[:,grade]>= before) & (model.loc[:,grade]< after)]
+        #This one is for the histogram
+        freq = data_plot.shape[0]
+        data_plot.loc[:,grade] =data_plot.loc[:,grade].round(3)
+        data_plot.loc[:,'g'] = '{}: '.format(grade) + data_plot.loc[:,grade].astype(str)
+        #naming the plot on the legend
+        name_legend = str(before) + " <=" + grade +"< " +str(after)
+        fig.add_trace(go.Scatter3d(x = data_plot.loc[:,xcol], y = data_plot.loc[:,ycol], z = data_plot.loc[:,zcol], text= data_plot.loc[:,'g'],mode = 'markers'\
+                                , name = name_legend, marker = dict(color = colors[i], symbol = 'square', size = 4), showlegend = True))
+        fig.add_trace(go.Bar(x = [name_legend], y = [freq], marker=dict(color = colors[i]), showlegend = False, name = '', text = [freq], textposition = 'auto'), row = 5, col=1)
+        i+=1
+
+    #Include the precedence in the title!!!!!
+    fig.update_layout(title = '<b>Ultimate Pit Limit</b>' + '<br>'+ \
+                  '<i>Undiscounted value ot the pit: </i>'+ '<b>USD {0:,.2f}</b>'.format(model.iloc[:,4].sum()),
+                 annotations = [{'font': {'size':14},'text': '<b>Histogram for grades within the ultimate pit limit</b>', 'x': 0.46, 'showarrow': False, 'y': 0.40, 
+                                    'xanchor': 'center', 'xref': 'paper', 'yanchor':'bottom', 'yref': 'paper'}],
+                 yaxis2 = {'anchor': 'x2', 'domain': [0.0, 0.5]},
+                 legend = dict(x = 1.0, y = 1.0, font = dict(size = 13)))
+
+    fig.update_yaxes(title_text = 'Frequency', row = 5, col = 1)
+    fig.update_xaxes(title_text = 'Grade Ranges', row = 5, col = 1)
+    return fig
+if __name__ ==  '__main__':
+    main()
---
+++ back.py
@@ -0,0 +1,187 @@
+import streamlit as st
+import pseudoflow as pf
+import numpy as np
+import networkx as NetX 
+import plotly.express as px
+import plotly.graph_objects as go 
+from scipy import spatial
+from statistics import mode 
+import pandas as pd 
+from PIL import Image
+from plotly.subplots import make_subplots
+
+#transfering csv w/blockmodel to backend
+class blockmodel(object):
+    def __init__(self, bmodel):
+        self.bmodel = bmodel
+        
+    def columns(self):
+        return self.bmodel.columns.values.tolist()
+
+    def summary(self, x, y, z, grade, density):
+        def data():
+            #See that index of x starts at 0 then... 1!
+            self.x_axis = x
+            self.y_axis = y
+            self.z_axis = z
+            self.gr_axis = grade
+            self.ds_axis = density
+            self.bmodel = self.bmodel.loc[:, [self.x_axis, self.y_axis,self.z_axis, self.gr_axis, self.ds_axis]]
+            self.x = self.bmodel.loc[:, x]
+            self.y = self.bmodel.loc[:, y]
+            self.z = self.bmodel.loc[:, z]
+            self.grade = self.bmodel.loc[:, grade]
+            self.density = self.bmodel.loc[:, density]
+            self.xlong = len(np.unique(self.x))
+            self.ylong = len(np.unique(self.y))
+            self.zlong = len(np.unique(self.z))
+            self.long = self.xlong * self.ylong * self.zlong
+            self.source = 0
+            self.minx = min(self.x)
+            self.maxx = max(self.x)
+            self.miny = min(self.y)
+            self.maxy = max(self.y)
+            self.minz = min(self.z)
+            self.maxz = max(self.z)
+            self.min_com = min(self.xlong, self.ylong)//2
+            self.sink = np.int(1 + self.long)
+            self.xunique = self.x.unique()
+            self.yunique = self.y.unique()
+            self.zunique = self.z.unique()
+            self.grademin = min(self.grade)
+            self.grademax = max(self.grade)
+            self.modex = int(mode(np.diff(np.unique(self.x))))
+            self.modey = int(mode(np.diff(np.unique(self.x))))
+            self.modez = int(mode(np.diff(np.unique(self.x))))   
+  
+        return data
+    
+    #Say go or not given the #rows
+    def summary_2(self):
+        return self.bmodel.shape[0]
+
+    #Outliers are in csv by purpose
+    def cleanning(self):
+        self.outliers = []
+        cols = [self.x_axis, self.y_axis, self.z_axis, self.gr_axis, self.ds_axis]
+
+        mean_x, std_x = np.mean(self.x), np.std(self.x)
+        mean_y, std_y = np.mean(self.y), np.std(self.y)
+        mean_z, std_z = np.mean(self.z), np.std(self.z)
+
+        #threshold for sd and mean
+        threshold = 2
+        for x,y,z,g,dens in zip(self.x, self.y, self.z, self.grade, self.density):
+            x_score= (x - mean_x)/std_x
+            y_score= (y - mean_y)/std_y
+            z_score= (z - mean_z)/std_z
+            x_up = x + self.modex
+            x_low = x - self.modex
+            y_up = y + self.modey
+            y_low = y - self.modey
+            z_up = z + self.modez
+            z_low = z - self.modez
+            if np.abs(x_score) > threshold or np.abs(y_score) > threshold or np.abs(z_score) > threshold:
+                if type(g) == list:
+                    self.outliers.append([x,y,z, g[0],g[1], dens])
+                else:
+                    self.outliers.append([x,y,z, g, dens])
+            elif (x_up not in self.xunique and x_low not in self.xunique) or (y_up not in self.yunique and y_low not in self.yunique) or \
+                    (z_up not in self.zunique and z_low not in self.zunique):
+                if type(g) == list:
+                    self.outliers.append([x,y,z, g[0],g[1], dens])
+                else:
+                    self.outliers.append([x,y,z, g, dens])
+        out = np.array(self.outliers)
+        
+        for a,b,c in zip(out[:,0], out[:,1], out[:,2]):
+            delete = self.bmodel.loc[(self.x == a) & (self.y == b) & (self.z == c)]
+            self.bmodel = self.bmodel.drop(delete.index)
+        
+         
+        
+        return pd.DataFrame(self.outliers, columns = cols)
+
+    #Solving UPL problem
+    def upl(self, x,y,z,grade, density, mc, ic, pc, tc, mp, mr, prec):
+        new_m = self.bmodel
+
+        max_z = self.maxz
+        if prec == '1-5 pattern':
+            prec = 5
+        elif prec == '1-9 pattern':
+            prec = 9
+        b_val = 'bvalue'
+        c_off = 'cutoff'
+        dic = 'dictator'
+        node = 'node'
+        new_m.loc[:,c_off] = np.array((mc + pc + (max_z - new_m.loc[:,z])*ic)/
+                                ((mp - tc) * mr * 22.04))
+                                
+
+        new_m.loc[:,b_val] = np.where((new_m.loc[:,grade] > new_m.loc[:, c_off]), 
+                    new_m.loc[:,'ton'] * (((mp - tc) * 22.04 * mr* new_m.loc[:,grade]) - 
+                                    pc - mc - (max_z - new_m.loc[:,z])*ic)
+                    , (0 - new_m.loc[:,'ton'] * mc))
+        new_m = new_m.reset_index(drop = True)
+        new_m.loc[:, node] = new_m.index + 1
+        new_m.loc[:,dic] = 0
+        #We get the new_m fot getting upl
+        #node:0, x:1, y:2, z:3, b_val:4, dic:5
+        new_m = new_m[[node, x, y, z, b_val, dic, density, grade]]
+        Graph = self.get_graph(nm = new_m, prec = prec)
+        breakpoints, cuts, info = pf.hpf(Graph, self.source, self.sink, const_cap="const", mult_cap="mult", lambdaRange=[0], roundNegativeCapacity=False)
+        
+        #Going over the cuts_items finding the nodes inside the resulting UPL.
+        Result_upl = {x:y for x, y in cuts.items() if y == [1] and x!=0}
+        InsideList = list(Result_upl.keys())
+        
+        # Set all blocks as zero
+
+        for ind in InsideList: 
+            # Set blocks inside UPL as one
+            new_m.loc[np.int(ind -1), dic] = 1
+
+        return new_m[new_m.loc[:,dic]==1]
+
+    def get_graph(self, nm, prec):      
+        Graph = NetX.DiGraph()
+        col_compare = self.zunique[::-1]
+        if prec == 9:
+            dista = (self.modex**2 + self.modey**2+ self.modez**2)**0.5
+        elif prec == 5:
+            dista = (self.modex**2 + self.modez**2)**0.5
+
+        for i in range(self.min_com):
+            #node:0, x:1, y:2, z:3, b_val:4, dic:5
+            upper = np.array(nm[nm.iloc[:,3]== col_compare[i]])
+            lower = np.array(nm[nm.iloc[:,3] == col_compare[i+1]])
+            #Watch out with index block value
+            self.CreateArcs(Graph = Graph, up = upper, low = lower, trigger = i, prec = prec, dist=dista)
+            
+            nm = nm[(nm.iloc[:,1]!= self.minx + i*self.modex) & (nm.iloc[:,1] != self.maxx - i*self.modex) & (nm.iloc[:,2] != self.miny + i*self.modey) & (nm.iloc[:,2] != self.maxy - i*self.modey)]
+        return Graph
+
+    def CreateArcs(self,Graph,up, low, trigger, prec, dist):
+        #Create internal arcs:
+        tree_upper = spatial.cKDTree(up[:,1:4])
+        mask = tree_upper.query_ball_point(low[:,1:4], r = dist + 0.01)
+        for _, g in enumerate(mask):
+            if len(g) == prec:
+                for reach in up[g][:,0]:
+                    Graph.add_edge(low[_][0], reach, const = 99e9, mult = 1)
+                
+        #Create external arcs:
+        #if trigger+2  == min_compare:
+        #  player = low
+        #else:
+        player = up
+        for node, capacity in zip(player[:,0], player[:,4]):
+            cap_abs = np.absolute(np.around(capacity, decimals=2))
+            if capacity < 0:
+                Graph.add_edge(node, self.sink, const = cap_abs, mult = -1)
+            else:
+                Graph.add_edge(0, node, const = cap_abs, mult = 1)
+
+
+        
