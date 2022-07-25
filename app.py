066f14d06260c7bc46c7c4a6148200387c19554e
luisflarota <lflarota@uc.cl>
2022-07-24 18:17:35 -0600

    Fix: Deleting plotly express to deploy app

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from statistics import mode
 
 import numpy as np
 import pandas as pd
-import plotly.express as px
 import plotly.graph_objects as go
 import streamlit as st
 from plotly.subplots import make_subplots
